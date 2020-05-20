8df4f76e47ccb91edb7cff32df4d4095b58fcc5b
lstorm-ux <63728542+lstorm-ux@users.noreply.github.com>
2020-05-20 10:07:38 +0200

    1#

---
+++ main.py
@@ -0,0 +1,10 @@
+
+input = 'IT is A RainY DaY'
+
+print("String with upper case letters:")
+print(input)
+
+# lower() function to convert
+# string to lower_case
+print("\nConverted String with lower case letters:")
+print(input.lower())
